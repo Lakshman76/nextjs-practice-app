1d643dc05563126a2fbf4ee093ffbac7ae3011b7
Lakshman76 <lakshmankumar2603@gmail.com>
2024-10-24 12:00:11 +0530

    dynamic routes

---
+++ app/products/page.tsx
@@ -0,0 +1,17 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Product lists</h1>
+      <h3>Product 1</h3>
+      <h3>Product 2</h3>
+      <h3>Product 3</h3>
+      <h3>Product 4</h3>
+      <h3>Product 5</h3>
+      <h3>Product 6</h3>
+      <h3>Product 7</h3>
+      <h3>Product 8</h3>
+      <h3>Product 9</h3>
+      <h3>Product 10</h3>
+    </div>
+  );
+}
