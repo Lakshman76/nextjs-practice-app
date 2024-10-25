d60202b8959920198d217c1baddd56f4f213c49b
Lakshman76 <lakshmankumar2603@gmail.com>
2024-10-25 12:08:18 +0530

    private folder or routing

---
+++ app/_password/page.tsx
@@ -0,0 +1,8 @@
+export default function SensitiveData() {
+  return (
+    <div>
+      <h1>Sensitive Data</h1>
+      <p>You cannot access this page in the browser</p>
+    </div>
+  );
+}
---
+++ app/_password/password.ts
@@ -0,0 +1,1 @@
+// All my passwords are here
