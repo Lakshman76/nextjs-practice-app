1aab9b9daddc4fc0b81b9c8510c398a7e0c570a4
Lakshman76 <lakshmankumar2603@gmail.com>
2024-10-25 12:00:47 +0530

    file colocation

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import LineChart from "./line-chart";
 
-export default function Page() {
-    return (
-      <div>
-        <h1>Hello from dashboard</h1>
-      </div>
-    );
-  }
-  
+function BarChart() {
+  return <h1>Bar Chart</h1>;
+}
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Hello from dashboard</h1>
+      <BarChart />
+      <LineChart />
+    </div>
+  );
+}
