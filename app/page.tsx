a8fcd7b1cf5c82c8c74c4034e4f4929f30c935f5
Lakshman76 <lakshmankumar2603@gmail.com>
2024-10-27 11:24:32 +0530

    components navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
 
 export default function Page() {
   return (
     <div>
-      <h1>Hello Next.js</h1>
+      <h1 className="text-3xl font-bold">Welcome Home!</h1>
+      <Link href="/blog" className="text-blue-500 underline">
+        Read blog
+      </Link>
+      <br />
+      <Link href="/products" className="text-blue-500 underline">
+        Products
+      </Link>
     </div>
   );
 }
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,28 @@
+import Link from "next/link";
+
 export default function Products() {
+  const productId = 100;
   return (
     <div>
-      <h1>Product lists</h1>
-      <h3>Product 1</h3>
-      <h3>Product 2</h3>
-      <h3>Product 3</h3>
+      <Link href="/" className="text-blue-500 underline">
+        Back to Home
+      </Link>
+      <h1 className="text-3xl font-bold">Product lists</h1>
+      <h3>
+        <Link href="/products/1" className="text-blue-500 underline" replace>
+          Product 1
+        </Link>
+      </h3>
+      <h3>
+        <Link href="/products/2" className="text-blue-500 underline">
+          Product 2
+        </Link>
+      </h3>
+      <h3>
+        <Link href="/products/3" className="text-blue-500 underline">
+          Product 3
+        </Link>
+      </h3>
       <h3>Product 4</h3>
       <h3>Product 5</h3>
       <h3>Product 6</h3>
@@ -12,6 +30,14 @@ export default function Products() {
       <h3>Product 8</h3>
       <h3>Product 9</h3>
       <h3>Product 10</h3>
+      <h3>
+        <Link
+          href={`/products/${productId}`}
+          className="text-blue-500 underline"
+        >
+          Product {productId}
+        </Link>
+      </h3>
     </div>
   );
 }
